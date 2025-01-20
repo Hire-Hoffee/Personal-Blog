b2a293c437483ebf2b834334ede65e8c5b0db1c2
Rostislav Alekseev <mr.rostik.01@gmail.com>
2025-01-20 12:45:24 +0200

    some styling

--- client/src/components/Notification.tsx
+++ client/src/components/Notification.tsx
@@ -1,4 +1,4 @@
-import { Snackbar } from "@mui/material";
+import { Alert, Snackbar } from "@mui/material";
 
 type Props = {
   open: boolean;
@@ -8,12 +8,11 @@ type Props = {
 
 function Notification({ open, onClose, message }: Props) {
   return (
-    <Snackbar
-      open={open}
-      onClose={onClose}
-      autoHideDuration={5000}
-      message={message || "Что-то произошло..."}
-    />
+    <Snackbar open={open} onClose={onClose} autoHideDuration={5000}>
+      <Alert onClose={onClose} severity="error" variant="filled" sx={{ width: "100%" }}>
+        {message || "Что-то произошло..."}
+      </Alert>
+    </Snackbar>
   );
 }
 
